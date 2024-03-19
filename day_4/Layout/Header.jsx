2b7e3eecbc755cf350c580140a824d1e3330f886
mahisapugal <110533310+mahisapugal@users.noreply.github.com>
2024-03-19 12:37:52 +0530

    Add files via upload

---
+++ day_4/Layout/Header.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Logo from "../LoginAssets/logo.png";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import { NavLink } from "react-router-dom";
+import "../styles/HStyles.css";
+const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  // hndle menu click
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  //menu drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography
+        color={"goldenrod"}
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1, my: 2 }}
+      >
+        
+      </Typography>
+      <Divider />
+      <ul className="mobile-navigation">
+        <li>
+          <NavLink activeClassName="active" to={"/userhome"}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/list"}>List</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/about"}>About</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/contact"}>Contact</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/"}>Logout</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/"}>Calculate</NavLink>
+        </li>
+      </ul>
+    </Box>
+  );
+  return (
+    <>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{
+                mr: 2,
+                display: { sm: "none" },
+              }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <NavLink activeClassName="active" to={"/userhome"}>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/list"}>List</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/about"}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/contact"}>Contact</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/"}>Logout</NavLink>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: "240px",
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box>
+          <Toolbar />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
---
+++ day_4/data/data.jsx
@@ -0,0 +1,52 @@
+import i1 from "../Assets/i1.jpg";
+import i2 from "../Assets/i2.jpg";
+import i3 from "../Assets/i3.jpg";
+import i4 from "../Assets/i4.jpg";
+import i5 from "../Assets/i5.jpg";
+import i6 from "../Assets/i6.jpg";
+
+
+export const ListC = [
+  {
+    name: "i1",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: i1,
+    price: 200,
+  },
+  {
+    name: "i2",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: i2,
+    price: 250,
+  },
+  {
+    name: "i3",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: i3,
+    price: 300,
+  },
+  {
+    name: "i4",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: i4,
+    price: 100,
+  },
+  {
+    name: "i5",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: i5,
+    price: 400,
+  },
+  {
+    name: "i6",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: i6,
+    price: 500,
+  },
+];
